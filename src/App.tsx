1538fb5e1881c110a5c788f3e707c68da4bd3bc3
Lucas Frassao <frassao.lucas@gmail.com>
2020-09-18 08:05:29 -0300

    investments page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import '../node_modules/foundation-sites/dist/css/foundation.min.css';
-import GlobalStyle from './styles/globalStyles';
+import GlobalStyle from './styles/global';
+
+import Investments from './pages/Investments';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <Investments />
 
       <GlobalStyle />
     </>
---
+++ src/pages/Investments/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Investments: React.FC = () => {
+  return (
+    <Container>
+      <h1>Investments page</h1>
+    </Container>
+  );
+};
+
+export default Investments;
