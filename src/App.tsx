03c3d762a1e6b3e3e369bdfc18b2512ac91b849c
Lucas Frassao <frassao.lucas@gmail.com>
2020-09-18 21:23:04 -0300

    added browser router if Link is needed so it doesn't reaload the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import './assets/foundation-6.6.3-custom/css/foundation.min.css';
 import GlobalStyle from './styles/global';
@@ -7,11 +8,11 @@ import Investments from './pages/Investments';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <Investments />
 
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 };
 
